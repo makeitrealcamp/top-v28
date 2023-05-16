de286f81d68139a52e48c8bdf2a0e9db6dea42cc
Gustavo Morales <gustavo.morales@gmail.com>
2023-05-15 17:47:45 -0700

    Add CRUD functions for Songs resource

--- projects/intro/winamp-api/index.js
+++ projects/intro/winamp-api/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 const port = 3000;
+
+const songs = [];
 
 /**
  * C(reate), R(ead), U(pdate) and D(elete)
  * GET, POST, PUT, DELETE
+ *
+ * ENDPOINTS
  *
  * POST    /songs      CREATE
  * GET     /songs      READ (ALL)
@@ -13,15 +19,102 @@ const port = 3000;
  * DELETE  /songs/:id  DELETE
  */
 
-app.get('/', (req, res, next) => {
-  res.json({
-    message: 'Welcome to the API',
+app.use(express.json());
+
+// CREATE
+app.post('/songs', async (req, res, next) => {
+  // body
+  const { body = {} } = req;
+
+  const song = {
+    ...body,
+    id: uuidv4(),
+    // title: body.title,
+    // author: body.author,
+  };
+
+  songs.push(song);
+
+  res.status(201);
+  res.json(song);
+});
+
+// READ ALL
+app.get('/songs', (req, res, next) => {
+  res.json(songs);
+});
+
+// READ ONE
+app.get('/songs/:id', (req, res, next) => {
+  // params
+  const { params = {} } = req;
+  const { id = '' } = params;
+
+  const song = songs.find(function (element) {
+    return id === element.id;
   });
+
+  if (song) {
+    res.json(song);
+  } else {
+    next({
+      statusCode: 404,
+      message: `Song with ${id}, Not Found`,
+    });
+  }
+});
+
+// UPDATE
+app.put('/songs/:id', (req, res, next) => {
+  // params
+  const { params = {}, body = {} } = req;
+  const { id = '' } = params;
+
+  const index = songs.findIndex(function (element) {
+    return id === element.id;
+  });
+
+  if (index !== -1) {
+    const song = {
+      ...songs[index],
+      ...body,
+    };
+
+    songs[index] = song;
+
+    res.json(song);
+  } else {
+    next({
+      statusCode: 404,
+      message: `Song with ${id}, Not Found`,
+    });
+  }
+});
+
+// DELETE
+app.delete('/songs/:id', (req, res, next) => {
+  const { params = {} } = req;
+  const { id = '' } = params;
+
+  const index = songs.findIndex(function (element) {
+    return id === element.id;
+  });
+
+  if (index !== -1) {
+    songs.splice(index, 1);
+    res.status(204);
+    res.end();
+  } else {
+    next({
+      statusCode: 404,
+      message: `Song with ${id}, Not Found`,
+    });
+  }
 });
 
 app.use((req, res, next) => {
   next({
-    statusCode: 400,
+    statusCode: 404,
     message: 'Route Not Found',
   });
 });
