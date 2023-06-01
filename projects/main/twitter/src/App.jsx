b69c8590ad184255d9d574295be1910b487ac532
Gustavo Morales <gustavo.morales@gmail.com>
2023-05-31 17:27:44 -0700

    Add Bootstrap, Icons and Fonts

--- projects/main/twitter/src/App.jsx
+++ projects/main/twitter/src/App.jsx
@@ -1,5 +1,17 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
--- projects/main/twitter/src/main.jsx
+++ projects/main/twitter/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.jsx';
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
