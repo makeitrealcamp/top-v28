0f41880e77520eee264bcd648abf5c697fa72078
Gustavo Morales <gustavo.morales@gmail.com>
2023-05-31 17:58:38 -0700

    Add Navigation component

--- projects/main/twitter/src/App.jsx
+++ projects/main/twitter/src/App.jsx
@@ -1,14 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Container>
+    <Container fluid>
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col>
+          <Navigation />
+        </Col>
+        <Col md={6} className="border-start border-end"></Col>
+        <Col></Col>
       </Row>
     </Container>
   );
