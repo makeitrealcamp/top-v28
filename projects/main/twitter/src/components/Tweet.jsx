deb7b8255691582bae6727c9dc03d5b95b826538
Gustavo Morales <gustavo.morales@gmail.com>
2023-06-07 17:55:25 -0700

    Add propTypes to User and Tweet components

--- projects/main/twitter/src/components/Tweet.jsx
+++ projects/main/twitter/src/components/Tweet.jsx
@@ -1,41 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { formatNumber } from '../utils';
+import styled from '@emotion/styled';
 
-export default function Tweet() {
+const StatisticContainer = styled('div')(({ theme }) => ({
+  fontSize: theme.fonts[0],
+}));
+
+export default function Tweet({
+  content,
+  createdAt,
+  name,
+  photo,
+  username,
+  statistics,
+}) {
+  const { commentsCount, retweetsCount, likesCount, viewsCount } = statistics;
   return (
     <div className="d-flex gap-2 border-bottom py-3">
       <div className="d-flex">
         <div className="p-2">
-          <img src="https://placehold.co/40x40" className="rounded-circle" />
+          <img src={photo} className="rounded-circle" />
         </div>
       </div>
       <article className="d-flex flex-column gap-1">
         <header>
-          <strong>Gustavo Morales</strong>{' '}
-          <span className="text-secondary">@gmorales • 11h</span>
+          <strong>{name}</strong>{' '}
+          <span className="text-secondary">
+            @{username} • {createdAt}
+          </span>
         </header>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-          maiores! Aliquam eos voluptatem repudiandae dolor praesentium cumque
-          nemo aliquid voluptates vero? Sequi, nam.
-        </p>
+        <div>{content}</div>
         <footer className="d-flex justify-content-between">
-          <div>
-            <i className="bi bi-chat"></i> 144
-          </div>
-          <div>
-            <i className="bi bi-repeat"></i> 1,530
-          </div>
-          <div>
-            <i className="bi bi-heart"></i> 5,678
-          </div>
-          <div>
-            <i className="bi bi-bar-chart"></i> 1.3M
-          </div>
-          <div className="me-5">
+          <StatisticContainer>
+            <i className="bi bi-chat"></i> {formatNumber(commentsCount)}
+          </StatisticContainer>
+          <StatisticContainer>
+            <i className="bi bi-repeat"></i> {formatNumber(retweetsCount)}
+          </StatisticContainer>
+          <StatisticContainer>
+            <i className="bi bi-heart"></i> {formatNumber(likesCount)}
+          </StatisticContainer>
+          <StatisticContainer>
+            <i className="bi bi-bar-chart"></i> {formatNumber(viewsCount)}
+          </StatisticContainer>
+          <StatisticContainer className="me-5">
             <i className="bi bi-box-arrow-up"></i>
-          </div>
+          </StatisticContainer>
         </footer>
       </article>
     </div>
   );
 }
+
+Tweet.propTypes = {
+  content: PropTypes.string.isRequired,
+  createdAt: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  photo: PropTypes.string.isRequired,
+  statistics: PropTypes.shape({
+    commentsCount: PropTypes.number,
+    retweetsCount: PropTypes.number,
+    likesCount: PropTypes.number,
+    viewsCount: PropTypes.number,
+  }),
+  username: PropTypes.string.isRequired,
+};
+
+Tweet.defaultProps = {
+  content:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente deserunt nam similique in est, voluptatem, pariatur consequatur sit repudiandae necessitatibus possimus beatae dolorum fuga neque quo amet! Totam, autem.',
+  name: 'Gustavo Morales',
+  photo: 'https://placehold.co/40x40',
+  statistics: {
+    commentsCount: 2345,
+    retweetsCount: 0,
+    likesCount: 12345678,
+    viewsCount: 0,
+  },
+  username: 'gmoralesc',
+};
--- projects/main/twitter/src/components/User.jsx
+++ projects/main/twitter/src/components/User.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function User({ children }) {
+export default function User({ children, name, username, photo }) {
   return (
     <div className="d-flex gap-2 justify-content-between rounded-pill p-2">
       <div className="d-flex">
         <div className="p-2">
-          <img src="https://placehold.co/40x40" className="rounded-circle" />
+          <img src={photo} className="rounded-circle" />
         </div>
         <div className="d-flex flex-column">
-          <strong>Gustavo Morales</strong>
-          <span>@gmoralesc</span>
+          <strong>{name}</strong>
+          <span>@{username}</span>
         </div>
       </div>
       {children}
     </div>
   );
 }
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  photo: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+User.defaultProps = {
+  name: 'Gustavo Morales',
+  username: 'gmoralesc',
+  photo: 'https://placehold.co/40x40',
+};
