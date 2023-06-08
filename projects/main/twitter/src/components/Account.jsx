6c0921c5d643acb26bd4d35f4a2ff173eab8edd5
Gustavo Morales <gustavo.morales@gmail.com>
2023-06-07 17:06:44 -0700

    Add Style to Profile

--- projects/main/twitter/src/components/Account.jsx
+++ projects/main/twitter/src/components/Account.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 
 export default function Account() {
   return (
-    <div className="d-flex mt-2 py-4 border-top">
+    <div className="d-flex mt-2 py-4">
       <div className="d-flex flex-grow-1 flex-column">
         <span>New on Twitter?</span>
         <Button className="rounded-pill flex-grow-1 m-2 py-2 fs-6 text-white">
