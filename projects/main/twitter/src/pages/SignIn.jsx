ecb14775b384f0f20a887e051654dd756846f955
C4rlos9559 <carlos9559@gmail.com>
2023-06-16 18:24:15 -0500

    Twitter forms (#18)
    * Add Validation to SignIn
    
    * Added form validation to SignUp component (#21)
    
    ---------
    
    Co-authored-by: Gustavo Morales <gustavo.morales@gmail.com>
    Co-authored-by: Javier Chavez <122915269+GosuBoy@users.noreply.github.com>

--- projects/main/twitter/src/pages/SignIn.jsx
+++ projects/main/twitter/src/pages/SignIn.jsx
@@ -1,29 +1,90 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Formik, ErrorMessage } from 'formik';
+import { z } from 'zod';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
+
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(16),
+});
 
 export default function SignIn() {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
   return (
     <>
       <h1 className="fs-4 my-2 fw-bolder">Sign In</h1>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }}
+        validationSchema={toFormikValidationSchema(signInSchema)}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                className={touched.email && errors.email ? 'is-invalid' : ''}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                className={
+                  touched.password && errors.password ? 'is-invalid' : ''
+                }
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-        <Button
-          variant="primary"
-          type="submit"
-          className="rounded-pill text-white px-4"
-        >
-          Submit
-        </Button>
-      </Form>
+            <Button
+              variant="primary"
+              type="submit"
+              className="rounded-pill text-white px-4"
+              disabled={isSubmitting}
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
