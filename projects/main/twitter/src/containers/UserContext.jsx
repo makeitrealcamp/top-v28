8632bee1d1c373ed51f31e802f5843a2bdd35372
Gustavo Morales <gustavo.morales@gmail.com>
2023-06-16 18:00:25 -0700

    Add LocalStorage to UserProvider

--- projects/main/twitter/src/containers/UserContext.jsx
+++ projects/main/twitter/src/containers/UserContext.jsx
@@ -1,9 +1,32 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const UserContext = createContext({});
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState();
+
+  // Mounted
+  useEffect(() => {
+    const json = localStorage.getItem('user');
+    if (json) {
+      try {
+        const data = JSON.parse(json);
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, []);
+
+  // Updated
+  useEffect(() => {
+    if (user === null) {
+      localStorage.removeItem('user');
+    }
+    if (user !== undefined) {
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider
