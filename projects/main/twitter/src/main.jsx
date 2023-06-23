d4520289306d823ab2b75979729f059f45e101f9
Gustavo Morales <gustavo.morales@gmail.com>
2023-06-23 15:49:41 -0700

    Add i18n

--- projects/main/twitter/src/main.jsx
+++ projects/main/twitter/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import resources from './locales/en/translation.json';
 
 import theme from './theme';
 import App from './App.jsx';
@@ -9,6 +13,12 @@ import App from './App.jsx';
 import './theme/custom.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './theme/index.css';
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  resources,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- projects/main/twitter/src/pages/Home.jsx
+++ projects/main/twitter/src/pages/Home.jsx
@@ -1,13 +1,38 @@
 import { useContext } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import styled from '@emotion/styled';
+
 import Create from '../components/Create';
 import Tweet from '../components/Tweet';
 import UserContext from '../containers/UserContext';
 
+const Name = styled('strong')(({ theme }) => ({
+  color: theme.colors.primary,
+}));
+
 export default function Home() {
   const { user } = useContext(UserContext);
+  const { t } = useTranslation();
+
   return (
     <>
-      <h1 className="fs-4 my-2 fw-bolder">Home</h1>
+      <h1 className="fs-4 my-2 fw-bolder">{t('home.title')}</h1>
+      <p>
+        {t('home.header.tweets', {
+          count: 2,
+        })}
+      </p>
+      <p>
+        <Trans
+          i18nKey="home.message"
+          values={{
+            user: 'Javier',
+          }}
+          components={{
+            bold: <Name />,
+          }}
+        />
+      </p>
       {user && <Create />}
       <Tweet />
       <Tweet />
