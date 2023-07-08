b2698cd2bb25734be58e649a6f1ead7ecf41bdb3
Welinton Suarez <wesgdeveloper@gmail.com>
2023-07-07 19:55:36 -0500

    Integration Test Page SingUp javier-welinton

---
+++ projects/main/twitter/src/test/SignUp.spec.jsx
@@ -0,0 +1,70 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@emotion/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "../theme";
+import App from "../App";
+import { getUser } from "../fixtures/user.fixture";
+
+describe("SingUp", () => {
+  test("create the account", async () => {
+    const user = getUser();
+
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+
+    fireEvent.click(screen.getByText("Sign Up"));
+
+    expect(await screen.findByText("Personal information")).toBeTruthy();
+
+    const name = screen.getByPlaceholderText("Enter Name");
+    const username = screen.getByPlaceholderText(/Enter Username/i);
+    const biography = screen.getByPlaceholderText(/About you/i);
+    const location = screen.getByPlaceholderText(/Location/i);
+    const email = screen.getByPlaceholderText(/email/i);
+    const password = screen.getByPlaceholderText(/password/i);
+
+    fireEvent.change(name, {
+      target: {
+        value: user.name,
+      },
+    });
+    fireEvent.change(username, {
+      target: {
+        value: user.username,
+      },
+    });
+    fireEvent.change(biography, {
+      target: {
+        value: "test",
+      },
+    });
+    fireEvent.change(location, {
+      target: {
+        value: "location test",
+      },
+    });
+    fireEvent.change(email, {
+      target: {
+        value: user.email,
+      },
+    });
+
+    fireEvent.change(password, {
+      target: {
+        value: "12345678",
+      },
+    });
+
+    const submit = screen.getByText("Submit");
+
+    fireEvent.click(submit);
+
+    expect(await screen.findAllByText("Required").length).toBeUndefined();
+  });
+});
