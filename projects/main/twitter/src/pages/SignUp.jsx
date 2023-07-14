e1120f217d8017c7f2916312b5486c4af3a62a45
Gustavo Morales <gustavo.morales@gmail.com>
2023-07-14 16:20:20 -0700

    Revert "Revert "Test sign in fabio carlos""
    This reverts commit 3645d3ab423de4b281ddc28919ad7b982cafff50.

--- projects/main/twitter/src/pages/SignUp.jsx
+++ projects/main/twitter/src/pages/SignUp.jsx
@@ -10,8 +10,8 @@ const signUpSchema = z.object({
   biography: z.string(),
   location: z.string(),
   email: z.string().email(),
-  password: z.string().min(6).max(16)
-})
+  password: z.string().min(6).max(16),
+});
 
 export default function SignUp() {
   const initialValues = {
@@ -21,7 +21,7 @@ export default function SignUp() {
     location: '',
     email: '',
     password: '',
-  }
+  };
   return (
     <>
       <h1 className="fs-4 my-2 fw-bolder">Sign Up</h1>
@@ -33,143 +33,146 @@ export default function SignUp() {
         }}
         validationSchema={toFormikValidationSchema(signUpSchema)}
       >
-        {
-          ({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-              <Form onSubmit={handleSubmit}>
-                <h2 className="fs-5 my-4">Personal information</h2>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <h2 className="fs-5 my-4">Personal information</h2>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control 
-                    type="text"
-                    placeholder="Enter Name"
-                    name="name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                    className={touched.name && errors.name ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage
-                    name="name"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                name="name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                className={touched.name && errors.name ? 'is-invalid' : ''}
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type="text" 
-                    placeholder="Enter Username" 
-                    name="username"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.username}
-                    className={touched.username && errors.username ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+                className={
+                  touched.username && errors.username ? 'is-invalid' : ''
+                }
+              />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Biography</Form.Label>
-                  <Form.Control 
-                    as="textarea" 
-                    rows={2} 
-                    placeholder="About you" 
-                    name="biography"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.location}
-                    className={touched.biography && errors.biography ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage 
-                    name="biography"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Biography</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={2}
+                placeholder="About you"
+                name="biography"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.biography}
+                className={
+                  touched.biography && errors.biography ? 'is-invalid' : ''
+                }
+              />
+              <ErrorMessage
+                name="biography"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Location</Form.Label>
-                  <Form.Control 
-                    type="text"
-                    placeholder="Location" 
-                    name="location"  
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.location}
-                    className={touched.location && errors.email ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage 
-                    name="location"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Location</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Location"
+                name="location"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.location}
+                className={touched.location && errors.email ? 'is-invalid' : ''}
+              />
+              <ErrorMessage
+                name="location"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <h2 className="fs-5 my-4">Login information</h2>
+            <h2 className="fs-5 my-4">Login information</h2>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control 
-                    type="email"
-                    placeholder="Enter email"
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    className={touched.email && errors.email ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                className={touched.email && errors.email ? 'is-invalid' : ''}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control 
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    className={touched.password && errors.password ? 'is-invalid' : ''}
-                  />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                className={
+                  touched.password && errors.password ? 'is-invalid' : ''
+                }
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </Form.Group>
 
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="rounded-pill text-white px-4"
-                  disabled={isSubmitting}
-                >
-                  Submit
-                </Button>
-              </Form>
-          )
-        }
+            <Button
+              variant="primary"
+              type="submit"
+              className="rounded-pill text-white px-4"
+              disabled={isSubmitting}
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
       </Formik>
-      
     </>
   );
 }
---
+++ projects/main/twitter/src/test/Signup.spec.jsx
@@ -0,0 +1,87 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from '../theme';
+import App from '../App';
+import { getUser } from '../fixtures/user.fixture';
+
+describe('Sign in', () => {
+  test('redirects to home page after signing in', async () => {
+    const user = getUser();
+
+    // 1. Visit Home Page
+    const { debug } = render(
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>,
+    );
+
+    // 2. Click on 'Sign Up' button
+
+    fireEvent.click(screen.getByText('Sign Up'));
+
+    // 3. Visit 'Sign Up' page
+
+    expect(await screen.findByText('Personal information')).toBeTruthy();
+
+    // 4. Fill out form and Fill out field by field
+
+    const name = screen.getByPlaceholderText('Enter Name');
+    const username = screen.getByPlaceholderText(/Username/i);
+    const biography = screen.getByPlaceholderText(/About/i);
+    const location = screen.getByPlaceholderText(/location/i);
+    const email = screen.getByPlaceholderText(/email/i);
+    const password = screen.getByPlaceholderText(/password/i);
+
+    fireEvent.change(name, {
+      target: {
+        value: user.name,
+      },
+    });
+
+    fireEvent.change(username, {
+      target: {
+        value: user.username,
+      },
+    });
+
+    fireEvent.change(biography, {
+      target: {
+        value:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam voluptatum, nihil sit accusamus minima rem corrupti adipisci nobis, commodi ratione voluptate non rerum molestiae porro dicta et officiis magnam.',
+      },
+    });
+
+    fireEvent.change(location, {
+      target: {
+        value: 'Bogota',
+      },
+    });
+
+    fireEvent.change(email, {
+      target: {
+        value: user.email,
+      },
+    });
+
+    fireEvent.change(password, {
+      target: {
+        value: '12345678',
+      },
+    });
+
+    // 5. Click on Submit Button display no errors
+
+    const submit = screen.getByText('Submit');
+
+    fireEvent.click(submit);
+
+    //await new Promise((resolve) => setTimeout(resolve, 2000));
+    await waitFor(() => expect(screen.queryByText('Required')).toBeNull());
+
+    //debug();
+  });
+});
