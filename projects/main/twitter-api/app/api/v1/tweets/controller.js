aaee1946de6d66a582501f83486a38fb5e8a20fc
Gustavo Morales <gustavo.morales@gmail.com>
2023-07-24 17:59:15 -0700

    Add Prisma and Tweet Model

--- projects/main/twitter-api/app/api/v1/tweets/controller.js
+++ projects/main/twitter-api/app/api/v1/tweets/controller.js
@@ -1,20 +1,56 @@
-export const create = (req, res) => {
-  res.status(201);
-  res.json({
-    data: body,
-  });
+import { prisma } from '../../../database.js';
+
+export const create = async (req, res, next) => {
+  const { body = {} } = req;
+
+  try {
+    const result = await prisma.tweet.create({
+      data: body,
+    });
+
+    res.status(201);
+    res.json({
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const all = (req, res) => {
-  res.json({
-    data: [],
-  });
+export const all = async (req, res, next) => {
+  try {
+    const result = await prisma.tweet.findMany();
+
+    res.json({
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const read = (req, res) => {
-  res.json({
-    data: {},
-  });
+export const read = async (req, res, next) => {
+  const { params = {} } = req;
+  try {
+    const result = await prisma.tweet.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!result) {
+      return next({
+        message: 'Tweet not found',
+        status: 404,
+      });
+    }
+
+    res.json({
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const update = (req, res) => {
--- projects/main/twitter-api/index.js
+++ projects/main/twitter-api/index.js
@@ -2,9 +2,14 @@ import http from 'http';
 
 import { configuration } from './app/config.js';
 import { app } from './app/index.js';
+import { connect } from './app/database.js';
 
 const { port } = configuration.server;
 
+// Connect to database
+connect();
+
+// Create Web Server
 const server = http.createServer(app);
 
 server.listen(port, () => {
