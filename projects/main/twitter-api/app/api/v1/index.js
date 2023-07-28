6d554e9a819bf6194e65d2583ffa9d4e62b88e49
Gustavo Morales <gustavo.morales@gmail.com>
2023-07-28 16:35:31 -0700

    Add User resource

--- projects/main/twitter-api/app/api/v1/index.js
+++ projects/main/twitter-api/app/api/v1/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import { router as tweets } from './tweets/routes.js';
+import { router as users } from './users/routes.js';
 
 // eslint-disable-next-line new-cap
 export const router = Router();
 
 router.use('/tweets', tweets);
+router.use('/users', users);
