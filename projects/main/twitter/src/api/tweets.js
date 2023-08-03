f049006719b2a9aeece7e4779fe254d94934877c
Gustavo Morales <gustavo.morales@gmail.com>
2023-08-02 17:23:50 -0700

    Add Axios in the API Layer

--- projects/main/twitter/src/api/tweets.js
+++ projects/main/twitter/src/api/tweets.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 function transformTweet(item = {}) {
   return {
     ...item,
@@ -9,55 +11,52 @@ function transformTweet(item = {}) {
 }
 
 export async function getTweets() {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/tweets/`);
-  if (response.ok) {
-    const json = await response.json();
-    const data = json.data.map(transformTweet);
+  try {
+    const { data: response } = await axios.get(
+      `${import.meta.env.VITE_API_URL}/tweets/`,
+    );
+    const data = response.data.map(transformTweet);
 
     return {
       data,
-      meta: json.meta,
+      meta: response.meta,
     };
-  } else {
-    return Promise.reject('Network error');
+  } catch (error) {
+    return Promise.reject(error.message);
   }
 }
 
 export async function getTweet({ id }) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/tweets/${id}`);
-  if (response.ok) {
-    const json = await response.json();
-    const data = transformTweet(json.data);
+  try {
+    const { data: response } = await axios.get(
+      `${import.meta.env.VITE_API_URL}/tweets/${id}`,
+    );
+    const data = transformTweet(response.data);
 
     return {
       data,
     };
+  } catch (error) {
+    return Promise.reject(error.message);
   }
-  if (response.status === 404) {
-    return Promise.reject('Not found');
-  }
-  return Promise.reject('Network error');
 }
 
 export async function createTweet(payload) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/tweets/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ...payload,
-      userId: 'ee36e6ae-ee5e-4725-82ec-ca089bb419f8',
-    }),
-  });
-  if (response.ok) {
-    const json = await response.json();
-    const data = transformTweet(json.data);
+  try {
+    const { data: response } = await axios.post(
+      `${import.meta.env.VITE_API_URL}/tweets/`,
+      {
+        ...payload,
+        userId: 'ee36e6ae-ee5e-4725-82ec-ca089bb419f8',
+      },
+    );
+
+    const data = transformTweet(response.data);
 
     return {
       data,
     };
-  } else {
-    return Promise.reject('Network error');
+  } catch (error) {
+    return Promise.reject(error.message);
   }
 }
