d5d2c482c9be2ccb6e5875fac652d2573dc0699b
C4rlos9559 <carlos9559@gmail.com>
2023-08-11 18:09:04 -0500

    Sign up auth ->navigate to signin (#37)
    * Sign up auth to sign in
    
    * quotes correction to proyect settings
    
    ---------
    
    Co-authored-by: Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>

--- projects/main/twitter/src/api/users.js
+++ projects/main/twitter/src/api/users.js
@@ -20,3 +20,21 @@ export async function signIn({ email, password }) {
     return Promise.reject(error.message);
   }
 }
+export async function signUp({ name, username, email, password }) {
+  try {
+    const { data: response } = await http.post('/users/signup', {
+      name,
+      username,
+      email,
+      password,
+    });
+
+    const { data } = response;
+
+    return {
+      data,
+    };
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+}
