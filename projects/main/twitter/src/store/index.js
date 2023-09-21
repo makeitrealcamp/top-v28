0338855dfd67c76e1938ffdafe8ff54265d015e9
Gustavo Morales <gustavo.morales@gmail.com>
2023-09-20 17:39:16 -0700

    Migrate to Redux Toolkit

--- projects/main/twitter/src/store/index.js
+++ projects/main/twitter/src/store/index.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import userReducer from './userReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer, { initialValues as userInitialState } from './userReducer';
 
-const store = createStore(
-  userReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const reducer = {
+  user: userReducer,
+};
+
+const initialState = {
+  user: userInitialState,
+};
+
+const store = configureStore({
+  reducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  initialState,
+});
 
 store.subscribe(function () {
   const state = store.getState();
--- projects/main/twitter/src/store/userReducer.js
+++ projects/main/twitter/src/store/userReducer.js
@@ -8,17 +8,12 @@ if (rawState) {
   user = null;
 }
 
-const initialValues = {
-  user,
-};
+export const initialValues = user;
 
 export default function userReducer(state = initialValues, action) {
   switch (action.type) {
     case 'SET_USER': {
-      return {
-        ...state,
-        user: action.payload,
-      };
+      return action.payload;
     }
     default:
       return state;
