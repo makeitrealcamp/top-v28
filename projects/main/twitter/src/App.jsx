1465bb71a0119d938c3aa5f047c87c11e29b56bf
Gustavo Morales <gustavo.morales@gmail.com>
2023-09-27 17:42:36 -0700

    Protect routes and Add new Blank page (#68)

--- projects/main/twitter/src/App.jsx
+++ projects/main/twitter/src/App.jsx
@@ -11,6 +11,7 @@ import { UserProvider } from './containers/UserContext';
 import ProtectedRoute from './containers/ProtectedRoute';
 
 const Home = lazy(() => import('./pages/Home.jsx'));
+const Blank = lazy(() => import('./pages/Blank.jsx'));
 const Compose = lazy(() => import('./pages/Compose.jsx'));
 const SignIn = lazy(() => import('./pages/SignIn.jsx'));
 const SignUp = lazy(() => import('./pages/SignUp.jsx'));
@@ -33,7 +34,19 @@ function App() {
           <Col md={6} className="border-start border-end">
             <Suspense fallback={null}>
               <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signed" element={<Signed />} />
+                <Route path="/activate/:token" element={<Activate />} />
+                <Route path="/confirmation" element={<Confirmation />} />
+                <Route
+                  path="/home"
+                  element={
+                    <ProtectedRoute>
+                      <Home />
+                    </ProtectedRoute>
+                  }
+                />
                 <Route
                   path="/compose"
                   element={
@@ -42,15 +55,31 @@ function App() {
                     </ProtectedRoute>
                   }
                 />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/signed" element={<Signed />} />
-                <Route path="/activate/:token" element={<Activate />} />
-                <Route path="/confirmation" element={<Confirmation />} />
-                <Route path="/blue/success" element={<BlueSuccess />} />
-                <Route path="/blue" element={<Blue />} />
-                <Route path="/tweet/:id" element={<Post />} />
-                <Route path="*" element={<Home />} />
+                <Route
+                  path="/blue/success"
+                  element={
+                    <ProtectedRoute>
+                      <BlueSuccess />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/blue"
+                  element={
+                    <ProtectedRoute>
+                      <Blue />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/tweet/:id"
+                  element={
+                    <ProtectedRoute>
+                      <Post />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="*" element={<Blank />} />
               </Routes>
             </Suspense>
           </Col>
