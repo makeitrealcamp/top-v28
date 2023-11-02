a4cf952047e96959c643ad5e906a102f79f9309a
Gustavo Morales <gustavo.morales@gmail.com>
2023-11-01 17:08:44 -0700

    Add UserContext

---
+++ projects/main/twitter-mobile/containers/UserContext/state.js
@@ -0,0 +1,14 @@
+let user = null;
+// const json = localStorage.getItem('state');
+// if (json) {
+//   try {
+//     const data = JSON.parse(json);
+//     user = data.user;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+export const initialState = {
+  user,
+};
