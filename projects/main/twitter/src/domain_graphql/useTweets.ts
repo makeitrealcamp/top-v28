b884894177384a4aa0204788ee7078abf6baafff
shymmy <shymmywgarcia@gmail.com>
2023-11-22 16:02:48 -0500

    remove create twee mutation

--- projects/main/twitter/src/domain_graphql/useTweets.ts
+++ projects/main/twitter/src/domain_graphql/useTweets.ts
@@ -1,7 +1,6 @@
 import useSWR from 'swr';
 import { fetcher } from '../api/graphql/client';
 import {
-  CREATE_TWEET_MUTATION,
   GET_ALL_TWEETS_QUERY,
 } from '../api/graphql/tweets';
 
@@ -24,13 +23,7 @@ export default function useTweets() {
   const { data, error, mutate } = useSWR<any>(GET_ALL_TWEETS_QUERY,fetcher);
 
   async function create(input: FormData) {
-    const newTweet = await fetcher(CREATE_TWEET_MUTATION, { input });
-    mutate(
-      (prevData: any) => ({
-        tweets: [newTweet.createTweet, ...prevData.tweets],
-      }),
-      false
-    );
+   //
   }
 
 
